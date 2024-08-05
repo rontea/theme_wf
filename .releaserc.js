087d9810df22df92e358712711b9aa97bbca1c4d
rontea <dupelawebsite@gmail.com>
2024-08-05 16:17:40 -0400

    fix(releaserc): try to fix automated failed
    3

--- .releaserc.js
+++ .releaserc.js
@@ -22,13 +22,6 @@ module.exports = {
         }
       }
     ],
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: process.env.NPM_PUBLISH === 'true' || process.env.GITHUB_REF === 'refs/heads/main'
-      }
-    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
