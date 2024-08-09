1bf043a64fefe718fbc31b2c2b79234b8962d6a5
rontea <dupelawebsite@gmail.com>
2024-08-09 13:42:06 -0400

    feat(feat(idincrementor)): increament the id class
    This will be use in creating id, sample TD01 , TD02 ,TS01 and so on...

--- testsrc/utils/idTest.js
+++ testsrc/utils/idTest.js
@@ -3,3 +3,9 @@
 const IdIncrementor = require('../../src/utils/IdIncrementor');
 
 const increment = new IdIncrementor();
+
+const newId = increment.getNewId();
+
+console.log(newId);
+
+increment.checkData();
---
+++ testsrc/utils/utils.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const utils = require('../../src/utils/utils');
+
+const number = 1;
+
+const number2 = 3
+
+const number3 = 10;
+
+const number4 = 19;
+
+const number5 = 20;
+
+const number6 = 9;
+
+console.log(utils.padZero(number));
+
+console.log(utils.padZero(number2));
+
+console.log(utils.padZero(number3));
+
+console.log(utils.padZero(number4));
+
+console.log(utils.padZero(number5));
+
+console.log(utils.padZero(number6));
+
