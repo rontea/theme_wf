879d3dd729e4e1b215543073b38948d990105f32
rontea <dupelawebsite@gmail.com>
2024-09-09 10:50:09 -0400

    ci(tweet update only on main)

--- tweet.js
+++ tweet.js
@@ -1,4 +1,5 @@
 const { TwitterApi } = require('twitter-api-v2');
+const utils = require('./src/utils/utils');
 
 const client = new TwitterApi({
   appKey: process.env.CONSUMER_API_KEY,
@@ -9,9 +10,15 @@ const client = new TwitterApi({
 
 async function tweetRelease() {
   try {
-    const message = `New release has been added to https://github.com/rontea/theme_wf/releases`;
-    await client.v2.tweet(message);
-    console.log('Tweeted:', message);
+
+    const currentBranch = utils.getCurrentBranch()
+
+    if(currentBranch === 'main'){
+      const message = `New release has been added to https://github.com/rontea/theme_wf/releases`;
+      await client.v2.tweet(message);
+      console.log('Tweeted:', message);
+    }
+
   } catch (error) {
     console.error('Error posting tweet:', error);
   }
